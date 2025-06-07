043ef51ed625daa56edc97bf186043471cdf15eb
BLADE-AO <evaelfiie1y@gmail.com>
2025-06-07 20:05:33 +0100

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100080479775577","61555039084008"];
+		const permission = ["100080479775577","61555039084008","61575839792460"];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage("𝗕𝗥𝗢 𝗦𝗘𝗨𝗟𝗘 𝗠𝗢𝗡 𝗕𝗢𝗦𝗦𝗘 𝗕𝗟𝗔𝗖𝗞 𝗔 𝗟𝗔 𝗣𝗘𝗥𝗠𝗜𝗦𝗦𝗜𝗢𝗡 🐥⚡", event.threadID, event.messageID);
 		}
